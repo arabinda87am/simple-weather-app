97e14ee0c34c6b56cae85c5f3f5f414b0f729702
Arabinda Manna <arabinda87am@gmail.com>
2020-03-02 18:24:16 +0530

    view folder removed and public folder introduced

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT;
 
-app.get('/', (req, res) => res.sendFile('views/index.html', {root: __dirname }))
+// app.get('/', (req, res) => res.sendFile('views/index.html', {root: __dirname }))
 
+app.use(express.static('public'));
 app.listen(port, () => console.log(`Weather App listening on port ${port}!`))
