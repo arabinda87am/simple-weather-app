a6051811f80bda89a5f5a3e590563ef42e46462a
Arabinda Manna <arabinda87am@gmail.com>
2020-03-02 18:42:55 +0530

    body-perser added

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT;
 
-// app.get('/', (req, res) => res.sendFile('views/index.html', {root: __dirname }))
-
 app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser);
+
+app.post('/get-weather', (req, res) => {
+    console.log(`Post request to weather app`,req.body);
+    res.json({});
+});
+
 app.listen(port, () => console.log(`Weather App listening on port ${port}!`))
