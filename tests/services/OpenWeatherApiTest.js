e07da79383a5939ae92eecd8462de3a8eca03c84
Arabinda Manna <arabinda87am@gmail.com>
2020-03-05 18:51:39 +0530

    Test case implementation started

---
+++ tests/services/OpenWeatherApiTest.js
@@ -0,0 +1,8 @@
+const { OpenWeatherApi } = require("../services/OpenWeatherApi");
+const { assert } = require("chai");
+
+describe("OpenWeatherApi Unit Testing", () => {
+    it("testing getCurrentWeatherByCoordinates ", () => {
+
+    });
+});
