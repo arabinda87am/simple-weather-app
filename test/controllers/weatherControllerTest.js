d3e1b6f5144da763090bb5b83f791566c37ddb14
Arabinda Manna <arabinda87am@gmail.com>
2020-03-06 14:28:23 +0530

    Test case write started

---
+++ test/controllers/weatherControllerTest.js
@@ -0,0 +1,8 @@
+const { assert } = require("chai");
+const { weatherController } = require("../../controllers/weatherController");
+
+describe("weather controller Testing", () => {
+    it("testing getWeather function", () => {
+        assert.equal("Abc", "Abc");
+    });
+});
---
+++ test/services/OpenWeatherApiTest.js
@@ -0,0 +1,8 @@
+const { OpenWeatherApi } = require("../../services/OpenWeatherApi");
+const { assert } = require("chai");
+
+describe("OpenWeather Api Call Testing", () => {
+    it("testing getCurrentWeatherByCoordinates function", () => {
+        assert.equal("Abc", "Abc");
+    });
+});
