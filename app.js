c3ea3030641bf38818256e583d69dbf19c01974d
Arabinda Manna <arabinda87am@gmail.com>
2020-03-06 12:21:49 +0530

    ejs view rendering package added index.html file rename to index.ejs get-weather end point rename to weather endpoint

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const app = express()
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const { weatherRouter } = require('./routers/weatherRouter');
 
-app.use('/get-weather', weatherRouter);
+app.use('/weather', weatherRouter);
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
 
 app.listen(port, () => console.log(`Weather App listening on port ${port}!`))
