379073c8174f04822843bca1aac140c1e567cb72
Arabinda Manna <arabinda87am@gmail.com>
2020-03-06 14:53:00 +0530

    commented the test case for now

--- test/services/OpenWeatherApiTest.js
+++ test/services/OpenWeatherApiTest.js
@@ -1,8 +1,8 @@
 const { OpenWeatherApi } = require("../../services/OpenWeatherApi");
 const { assert } = require("chai");
 
-describe("OpenWeather Api Call Testing", () => {
-    it("testing getCurrentWeatherByCoordinates function", () => {
-        assert.equal("Abc", "Abc");
-    });
-});
+// describe("OpenWeather Api Call Testing", () => {
+//     it("testing getCurrentWeatherByCoordinates function", () => {
+//         assert.equal("Abc", "Abc"); //Implementation required
+//     });
+// });
