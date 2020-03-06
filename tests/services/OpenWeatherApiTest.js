28f84f74ce269d3afe60ccd678bc1ed700fbf09f
Arabinda Manna <arabinda87am@gmail.com>
2020-03-06 14:28:01 +0530

    tests folder renamed to tests folder

--- tests/services/OpenWeatherApiTest.js
+++
@@ -1,8 +0,0 @@
-const { OpenWeatherApi } = require("../services/OpenWeatherApi");
-const { assert } = require("chai");
-
-describe("OpenWeatherApi Unit Testing", () => {
-    it("testing getCurrentWeatherByCoordinates ", () => {
-
-    });
-});
