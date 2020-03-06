dd0d9ef94add3d663341b0e1c9846e21aea7a3d1
Arabinda Manna <arabinda87am@gmail.com>
2020-03-06 15:29:03 +0530

    Test assertion rectified

--- test/controllers/weatherControllerTest.js
+++ test/controllers/weatherControllerTest.js
@@ -3,10 +3,10 @@ const { getWeather } = require("../../controllers/weatherController");
 
 describe("weather controller Testing", () => {
     it("testing getWeather function", async () => {
-        assert.isObject(await getWeather({
-                "latitude": "22.5415116",
-                "longitude": "88.3493653"
-            })
-        );
+        const weatherData = await getWeather({
+                                "latitude": "22.5415116",
+                                "longitude": "88.3493653"
+                            });
+        assert.oneOf(weatherData.status, ["SUCCESS","ERROR"]);
     });
 });
