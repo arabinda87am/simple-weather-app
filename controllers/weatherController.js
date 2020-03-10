400eb7f0e5293bd342b334296a107c6e3cc9d804
Arabinda Manna <arabinda87am@gmail.com>
2020-03-10 14:47:24 +0530

    storing api call data to database

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -4,10 +4,22 @@ getWeather = async (data) => {
     if ((data.latitude !== undefined && data.latitude !== null) && (data.longitude !== undefined && data.longitude !== null)){
         try {
             let apiData = await OpenWeatherApi.getCurrentWeatherByCoordinates(data.latitude, data.longitude);
-            // console.log(apiData.data);
-            
-            return { "status": "SUCCESS", "data": apiData.data };
+            let responseData = apiData.data;
+            // console.log(responseData);
+            let { WeatherHistory } = require('../models/WeatherHistory');
+            WeatherHistory.then(() => {
+                    WeatherHistory.create({ 
+                    cityName: responseData.name, 
+                    latitude: responseData.coord.lat,
+                    longitude: responseData.coord.lon,
+                    temperature: responseData.main.temp,
+                    units: "metric"
+                }).then(res => console.log('Database Insert Successful ' + res.id))
+                .catch(error => console.log('Database Insert Error', error))
+            });
+            return { "status": "SUCCESS", "data": responseData };
         } catch (error){
+            console.log(error);
             return { "status": "ERROR", "data": "Api call failed. Check OpenWeatherApi.js file." };
         }
     }else{
