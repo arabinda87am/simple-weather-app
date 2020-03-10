40afb7f7855e5190e19e63bc41c4e8bc76cdfa53
Arabinda Manna <arabinda87am@gmail.com>
2020-03-10 11:05:46 +0530

    database connection implemented

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Sequelize = require('sequelize');
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_TYPE,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+exports.db = db;
